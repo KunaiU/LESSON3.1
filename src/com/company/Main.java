16a40c92a444e4b37524cbbc21411a1d0389f32a
UMAX <kbuonrakyoev@gmail.com>
2021-12-26 18:48:12 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+    public static int digit = 9;
+
+    public static void test(int num) {
+        System.out.println(num);
+        int a = 8;
+        System.out.println(a);
+        if (8 > 1) {
+            System.out.println(a);
+            int b = 11;
+            System.out.println(b);
+
+        }
+
+        System.out.println(digit);
+    }
+
+    public static void main(String[] args) {
+
+        digit = 19;
+        System.out.println(digit);
+
+        int [] nums;
+
+    }
+
+
+}
